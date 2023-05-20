04a8dd777f1a87fbe5247e427e84549c8f2a1aac
Nicolas Riveros <n.riverosv@uniandes.edu.co>
2023-05-20 00:04:58 -0500

    .

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,13 @@
 const baseUrl =
  'https://worldcup-c4988-default-rtdb.firebaseio.com/matches.json';
 
+ const baseUrl2 =
+ 'https://worldcup-c4988-default-rtdb.firebaseio.com/teams.json';
+
+
+
 export const environment = {
  production: false,
- baseUrl
+ baseUrl,
+ baseUrl2
 };
